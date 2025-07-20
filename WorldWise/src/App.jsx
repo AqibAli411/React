108ffac1d52f4bc9e4bfac091c537b59691ce15b
Aqib Ali <aqibhalepoto78@gmail.com>
2025-07-20 15:09:57 +0500

    Added nested Routers

--- WorldWise/src/App.jsx
+++ WorldWise/src/App.jsx
@@ -1,13 +1,37 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./pages/Homepage"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
+import City from "./components/City";
+import CountryList from "./components/CountryList";
 
+const BASE_URL = "http://localhost:5000";
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("unknown error has occurred");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -15,11 +39,24 @@ function App() {
         <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} >
-          <Route index element={<p>List of cities</p>}/>
-          <Route path="cities" element={<p>List of cities</p>}/>
-          <Route path="countries" element={<p>Countries</p>}/>
-          <Route path="map" element={<p>map</p>}/>
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities/:id"
+            element={<City cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="countries"
+            element={<CountryList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="map" element={<p>map</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>
@@ -27,4 +64,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- WorldWise/src/components/Map.jsx
+++ WorldWise/src/components/Map.jsx
@@ -1,6 +1,11 @@
+import { useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 
 function Map() {
+    const [searchParams , setSearchParams] = useSearchParams();
+
+    console.log(searchParams.get('lat'));
+
     return (
         <div className={styles.mapContainer}>
             Map
