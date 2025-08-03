c7726a9c88bd1aaea17c4949318eaf00695bb4f2
Aqib Ali <aqibhalepoto78@gmail.com>
2025-08-03 23:14:32 +0500

    Completed Real Time Rich Text Editor

---
+++ TextEditor/src/App.jsx
@@ -0,0 +1,9 @@
+// import MainEditor from "./MainEditor";
+import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+
+function App() {
+  // return <MainEditor/>;
+  return <SimpleEditor />;  
+}
+
+export default App;
---
+++ TextEditor/vite.config.js
@@ -0,0 +1,29 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  define: {
+    global: "globalThis",
+  },
+  optimizeDeps: {
+    include: ["sockjs-client", "@stomp/stompjs"],
+  },
+  server: {
+    proxy: {
+      "/ws": {
+        target: "http://localhost:8080",
+        ws: true,
+        changeOrigin: true,
+      },
+    },
+  },
+});
