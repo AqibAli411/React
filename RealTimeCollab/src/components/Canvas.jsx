48631f171ef923aa396a719e75c1f30e5d370ccc
Aqib Ali <aqibhalepoto78@gmail.com>
2025-08-14 19:52:06 +0500

    removed duplications

--- RealTimeCollab/src/components/Canvas.jsx
+++
@@ -1,7 +0,0 @@
-import CanvasManager from "../features/DrawingCanvas/CanvasManager";
-
-function Canvas({ isDarkMode }) {
-  return <CanvasManager isDarkMode={isDarkMode} />;
-}
-
-export default Canvas;
